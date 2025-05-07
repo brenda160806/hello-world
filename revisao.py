5d6f8ed4722f96b6564f15ae612f21065ede32d9
brenda160806 <brendakarolyne800@gmail.com>
2025-05-07 15:22:56 -0300

    criacao revisao

---
+++ revisao.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+ #exemplo de numero float(flutuante
+ peso = 89.5
+ print(peso)
+
+ #exemplo de numer inteiro
+ idade = 18
+ print(idade)
+
+#exemplo de variavel do tipo texto 
+nome =("maria")
+sobrenome = "silva"
+print (nome,sobrenome)
