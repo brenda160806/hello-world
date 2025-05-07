ac0b721b22b06189661b6dbd7f823efdf4f7090f
brenda160806 <brendakarolyne800@gmail.com>
2025-05-07 15:37:01 -0300

    corrigio o bug de espaço exemplo de comitacao

--- revisao.py
+++ revisao.py
@@ -1,17 +1,23 @@
 
+ #exemplo de numero float(flutuante[] 
+peso = 89.5
+print(peso)
 
-
-
-
- #exemplo de numero float(flutuante
- peso = 89.5
- print(peso)
-
- #exemplo de numer inteiro
- idade = 18
- print(idade)
+ #exemplo de numer inteiro 
+idade = 18
+print(idade)
 
 #exemplo de variavel do tipo texto 
 nome =("maria")
 sobrenome = "silva"
 print (nome,sobrenome)
+
+
+#exemplo se A é maior que B
+a= 5 
+b= 10 
+
+if a > b:
+    print ('A é maior que B')
+else 
+    print (' A é MENOR que B')
