1750121fa714d09987f45b7b02fe4c98c3e6aea9
brenda160806 <brendakarolyne800@gmail.com>
2025-05-08 15:32:55 -0300

    ATUALIZACAO  BASICA

---
+++ exercicio.py
@@ -0,0 +1,24 @@
+#voce foi contratado para um freela que solicita para o usuario quatro notas e faz a media 
+#1. criar inputs que rewcebera as notas nemerica (int{ 1 /8 / 10 }ou float [0.5 2.5 7.5 ])
+#2. calculo de media [soma de todas as notas /4]
+#3. fazer a verificacao se a media maior igual [>=] 5, aprovado,senao reprovado 
+ 
+ #calculo de media 
+nota1 - int( input(INSIRA A NOTA )
+#print (nota 1)
+
+nota2 = float (input (INSIRA A NOTA))
+
+nota3 = int(input (INSIRA A NOTA)
+#PRINT (NOTA3)
+
+NOTA4 = FLOAT(INPUT(INSIRA A NOTA ))
+#PRINT (NOTA 4)
+
+#CALCULO DE MNEDIA
+MEDIA = "NOTA 1 + NOTA 2 + NOTA 3 + NOTA 4" / 4
+#PRINT (" O VALOR DE SOMA ENTRE OS NUMEROS DECLARADO É:, SOMA ")
+
+#VERIFICAR SE A PESSOA FOI APROVADA OU REPROVADA
+"IF MEDIA >= 7:"
+PRINT ( "APROVADO")
